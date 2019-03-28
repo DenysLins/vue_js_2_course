a7427dcda5e7e2054873b01f8c4afdb77d8908f4
Denys Lins <dalins@stefanini.com>
2019-03-28 18:14:35 -0300

    Use v-bind directive

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'Hello Vue!'
+        title: 'Denys',
+        link: 'http://www.google.com'
     },
     methods: {
         changeTitle: function (event) {
             this.title = event.target.value;
         },
-
         sayHello: function () {
-            return 'Hello!!!'
+            return 'Hello ' + this.title + '!!!';
         }
     },
 });
