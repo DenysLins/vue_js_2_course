bfb10566e76d0f275bb9ddc79aaa1a8b618a40ee
Denys Lins <denyslins@gmail.com>
2019-03-29 18:26:02 -0300

    Create app uusing cli

---
+++ basic/index.js
@@ -0,0 +1,26 @@
+new Vue({
+  el: '#app',
+  data: {
+    title: 'Denys',
+    link: 'http://www.google.com',
+    finishedLink: '<a href="https://denyslins.com">Denys Lins</a>',
+    counter: 0,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    changeTitle: function (event) {
+      this.title = event.target.value;
+    },
+    sayHello: function () {
+      return 'Hello ' + this.title + '!!!';
+    },
+    increase: function (step, event) {
+      this.counter += step;
+    },
+    updateCoordinates: function (event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    }
+  },
+});
