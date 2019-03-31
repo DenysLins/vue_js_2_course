7dcf7044bcd5f9d108a9e6799768c1ad397e1454
Denys Lins <denyslins@gmail.com>
2019-03-31 16:09:07 -0300

    Add other merthods

---
+++ project1/app.js
@@ -0,0 +1,54 @@
+new Vue({
+  el: "#app",
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false
+  },
+  methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
+    specialAttack: function () {
+      this.playerHealth -= 20;
+      this.monsterHealth -= 30;
+    },
+    heal: function () {},
+    giveUp: function () {
+      this.gameIsRunning = false;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor((Math.random() * max) + 1), min);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! New game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You lost! New game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    }
+  }
+});
