c856cab287c831c2efc837efb4a713d6868c183d
Denys Lins <denyslins@gmail.com>
2019-04-07 17:45:46 -0300

    Add a second component

---
+++ vue-cli/src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Server status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: true
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = !this.status;
+    }
+  }
+};
+</script>
