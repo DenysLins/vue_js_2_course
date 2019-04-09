b903a6c04b4b3a780e2bd46f526cdc0704281640
Denys Lins <denyslins@gmail.com>
2019-04-08 23:11:09 -0300

    Accessing props throught .this

--- vue-cli/src/ServerStatus.vue
+++ vue-cli/src/ServerStatus.vue
@@ -2,6 +2,7 @@
   <div>
     <p>Server status: {{ status }}</p>
     <p>This name was sent from parent component: {{ name }}</p>
+    <p>This call a function accessing and reversing props throught .this: {{ switchName() }}</p>
     <hr>
     <button @click="changeStatus">Change Status</button>
   </div>
@@ -17,6 +18,12 @@ export default {
   methods: {
     changeStatus() {
       this.status = !this.status;
+    },
+    switchName() {
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
     }
   },
   props: ["name"]
