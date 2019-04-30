844eb3ff07b490c9e92da59962688c3d84b0ef71
Denys Lins <denyslins@gmail.com>
2019-04-29 23:43:11 -0300

    Using directives arguments and modifiers

--- directives/src/main.js
+++ directives/src/main.js
@@ -3,10 +3,21 @@ import App from "./App.vue";
 
 Vue.directive("highlight", {
   bind(el, binding, vnode) {
-    el.style.backgroundColor = "green";
+    // el.style.backgroundColor = "green";
+    // el.style.backgroundColor = binding.value;
+    let delayed = 0;
+    if (binding.modifiers["delayed"]) {
+      delayed = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg === "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delayed);
   }
 });
-
 new Vue({
   el: "#app",
   render: h => h(App)
