0a9a112c1490e33dab983daed8d219a465ff8c06
Denys Lins <denyslins@gmail.com>
2019-04-29 23:19:01 -0300

    Using simple custom directives

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -1,19 +1,28 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <p v-text="'Some Text'" />
+        <p v-html="'<strong>Some Strong Text</strong>'" /> <!-- Remember to sanitiza the html code! -->
+      </div>
     </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight>
+          Styled with custom directives
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {
+};
 </script>
 
 <style>
-
 </style>
--- directives/src/main.js
+++ directives/src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = "green";
+  }
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
