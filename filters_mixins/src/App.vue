4c7f150ffda33ab39cf3d46ffac8143bbe6ee0e6
Denys Lins <denyslins@gmail.com>
2019-05-09 21:39:44 -0300

    Using filters

---
+++ filters_mixins/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUppercase }}</p>
+        <p>{{ text | toLowercase }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: "Hello there!!!"
+    }
+  },
+  filters: {
+    toUppercase (value) {
+      return value.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style>
+</style>
