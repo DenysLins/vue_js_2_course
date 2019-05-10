250bb221f3c0bc4c499e18640502c8c11856dc72
Denys Lins <denyslins@gmail.com>
2019-05-09 22:06:03 -0300

    Using computed for better performance

--- filters_mixins/src/App.vue
+++ filters_mixins/src/App.vue
@@ -5,6 +5,16 @@
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUppercase }}</p>
         <p>{{ text | toLowercase }}</p>
+        <p>{{ text | toUppercase | toLowercase }}</p>
+        <hr>
+        <input v-model="filterText">
+        <ul>
+          <li
+            v-for="(fruit, index) in filteredFruits"
+            :key="index"
+          >{{ fruit }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -14,12 +24,21 @@
 export default {
   data () {
     return {
-      text: "Hello there!!!"
+      text: "Hello there!!!",
+      fruits: ["Apple", "Banana", "Mango", "Melon"],
+      filterText: ""
     }
   },
   filters: {
     toUppercase (value) {
       return value.toUpperCase();
+    }
+  },
+  computed: {
+    filteredFruits () {
+      return this.fruits.filter((e) => {
+        return e.match(this.filterText);
+      });
     }
   }
 }
