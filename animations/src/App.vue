b13db34e0272eff30df791002688aca3d5d1eeb9
Denys Lins <denyslins@gmail.com>
2019-05-26 17:36:15 -0300

    Add basic fade animation effect

---
+++ animations/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button
+          class="btn btn-info"
+          @click="show = !show"
+        >Show Alert</button>
+        <br><br>
+        <transition name="fade">
+          <div
+            class="alert alert-info"
+            v-if="show"
+          >This is an animated alert</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-enter-active {
+  transition: opacity 2s;
+}
+
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: opacity 2s;
+}
+</style>
