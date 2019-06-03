a5c65493ad75eef4a059dc007c007a322cbbb708
Denys Lins <dalins@stefanini.com>
2019-06-03 18:39:10 -0300

    Creating an animation in javascript

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -44,6 +44,7 @@
           @leave="leave"
           @after-leave="afterLeave"
           @leave-cancelled="leaveCancelled"
+          :css="false"
         >
           <div style="width: 100px; height: 100px; background-color: red" v-if="load"></div>
         </transition>
@@ -58,16 +59,27 @@ export default {
     return {
       show: true,
       load: true,
-      customAnimation: "fade"
+      customAnimation: "fade",
+      elementWidth: 100
     };
   },
   methods: {
     beforeEnter(el) {
       console.log("beforeEnter");
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + "px";
     },
     enter(el, done) {
       console.log("enter");
-      done();
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth + round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
     afterEnter(el) {
       console.log("afterEnter");
@@ -77,10 +89,20 @@ export default {
     },
     beforeLeave(el) {
       console.log("beforeLeave");
+      this.elementWidth = 300;
+      el.style.width = this.elementWidth + "px";
     },
     leave(el, done) {
       console.log("leave");
-      done();
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth - round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
     afterLeave(el) {
       console.log("afterLeave");
