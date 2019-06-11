9808b0b5488dbb95fb1509e4aaa7bc4c8d7d89b8
Denys Lins <denyslins@gmail.com>
2019-06-10 23:45:16 -0300

    Using third part flip animation

--- mini_app/src/App.vue
+++ mini_app/src/App.vue
@@ -1,45 +1,53 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 class="text-center">The Super Quiz</h1>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
-                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-        
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 class="text-center">The Super Quiz</h1>
+      </div>
     </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <component
+          :is="mode"
+          @answered="answered($event)"
+          @confirmed="mode = 'app-question'"
+          :class="customStyle"
+        ></component>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Question from './components/Question.vue';
-    import Answer from './components/Answer.vue';
+import Question from './components/Question.vue';
+import Answer from './components/Answer.vue';
 
-    export default {
-        data() {
-            return {
-                mode: 'app-question'
-            }
-        },
-        methods: {
-          answered(isCorrect) {
-              if (isCorrect) {
-                  this.mode = 'app-answer';
-              } else {
-                  this.mode = 'app-question';
-                  alert('Wrong, try again!');
-              }
-          }
-        },
-        components: {
-            appQuestion: Question,
-            appAnswer: Answer
-        }
+export default {
+  data () {
+    return {
+      mode: 'app-question',
+      customStyle: {}
     }
+  },
+  methods: {
+    answered (isCorrect) {
+      if (isCorrect) {
+        this.customStyle = {
+          'animated': true,
+          'flip': true
+        };
+        this.mode = 'app-answer';
+      } else {
+        this.mode = 'app-question';
+        alert('Wrong, try again!');
+      }
+    }
+  },
+  components: {
+    appQuestion: Question,
+    appAnswer: Answer
+  }
+}
 </script>
 
--- mini_app/src/components/Answer.vue
+++ mini_app/src/components/Answer.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="alert alert-success text-center">
-        <h1>That's Correct!</h1>
-        <hr>
-        <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
-    </div>
+  <div class="alert alert-success text-center">
+    <h1>That's Correct!</h1>
+    <hr>
+    <button
+      class="btn btn-primary"
+      @click="onNextQuestion"
+    >Next Question</button>
+  </div>
 </template>
-<style>
 
+<style>
 </style>
+
 <script>
 
-    export default{
-        methods: {
-            onNextQuestion() {
-                this.$emit('confirmed');
-            }
-        }
+export default {
+  methods: {
+    onNextQuestion () {
+      this.$emit('confirmed');
     }
+  }
+}
 </script>
