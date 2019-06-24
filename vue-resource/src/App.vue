71b014453166f5b14948d1a5da6f60f60c695739
Denys Lins <denyslins@gmail.com>
2019-06-24 14:47:30 -0300

    Using vue-resource post method

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -1,16 +1,48 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label for="name">Username</label>
+          <input type="text" class="form-control" id="name" v-model="user.username">
         </div>
+        <div class="form-group">
+          <label for="mail">Mail</label>
+          <input type="email" class="form-control" id="mail" v-model="user.mail">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        mail: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.user);
+      this.$http
+        .post("https://vue-resource-bcaec.firebaseio.com/data.json", this.user)
+        .then(
+          response => {
+            console.log(response);
+          },
+          error => {
+            console.log(error);
+          }
+        );
     }
+  }
+};
 </script>
 
 <style>
