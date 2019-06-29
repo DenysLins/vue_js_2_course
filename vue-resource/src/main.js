fe8a64bf85c3c22164d67dee629c519a06914efb
Denys Lins <denyslins@gmail.com>
2019-06-28 22:30:05 -0300

    Intercepting requests

--- vue-resource/src/main.js
+++ vue-resource/src/main.js
@@ -4,7 +4,12 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
-Vue.http.options.root = 'firebase_url'
+Vue.http.options.root = 'firebase_endpoint'
+Vue.http.interceptors.push((request, next) => {
+  console.log(request)
+  if (request.method == 'POST') request.method = 'PUT'
+  next()
+})
 
 new Vue({
   el: '#app',
