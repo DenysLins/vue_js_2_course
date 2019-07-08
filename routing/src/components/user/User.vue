7a380c528707704353b64a80d47de3ed717ab492
Denys Lins <dalins@stefanini.com>
2019-07-08 18:27:58 -0300

    Add dinamically routing

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>The User Page</h1>
+  <div>
+    <h1>The User Page</h1>
+    <button class="btn btn-primary" @click="navigateToHome">Go Home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+      this.$router.push("/");
+    }
+  }
+};
+</script>
+
