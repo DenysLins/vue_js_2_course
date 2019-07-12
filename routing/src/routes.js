56b3834addae54f14c4e255bc79327077c48576a
Denys Lins <denyslins@gmail.com>
2019-07-11 22:47:54 -0300

    Using lazy loading and grouping them

--- routing/src/routes.js
+++ routing/src/routes.js
@@ -1,9 +1,44 @@
 import Home from "./components/Home.vue";
-import User from "./components/user/User.vue";
-import UserStart from "./components/user/UserStart.vue";
-import UserDetail from "./components/user/UserDetail.vue";
-import UserEdit from "./components/user/UserEdit.vue";
-import { runInNewContext } from "vm";
+
+const User = resolve => {
+  require.ensure(
+    ["./components/user/User.vue"],
+    () => {
+      resolve(require("./components/user/User.vue"));
+    },
+    "user"
+  );
+};
+
+const UserStart = resolve => {
+  require.ensure(
+    ["./components/user/UserStart.vue"],
+    () => {
+      resolve(require("./components/user/UserStart.vue"));
+    },
+    "user"
+  );
+};
+
+const UserDetail = resolve => {
+  require.ensure(
+    ["./components/user/UserDetail.vue"],
+    () => {
+      resolve(require("./components/user/UserDetail.vue"));
+    },
+    "user2"
+  );
+};
+
+const UserEdit = resolve => {
+  require.ensure(
+    ["./components/user/UserEdit.vue"],
+    () => {
+      resolve(require("./components/user/UserEdit.vue"));
+    },
+    "user2"
+  );
+};
 
 export const routes = [
   {
