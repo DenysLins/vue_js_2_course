16d09b27c560c1c9e3183d5b839bc87e6b0386df
Denys Lins <denyslins@gmail.com>
2019-07-13 23:04:54 -0300

    Mapping actions to methods and using payloads

--- vuex/src/components/AnotherCounter.vue
+++ vuex/src/components/AnotherCounter.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <button class="btn btn-primary" @click="asyncIncrement">Increment</button>
-    <button class="btn btn-primary" @click="asyncDecrement">Decrement</button>
+    <button class="btn btn-primary" @click="asyncIncrement({by: 50, duration: 2000})">Increment</button>
+    <button class="btn btn-primary" @click="asyncDecrement({by: 50, duration: 2000})">Decrement</button>
   </div>
 </template>
 
--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -17,29 +17,29 @@ export const store = new Vuex.Store({
     }
   },
   mutations: {
-    increment: state => {
-      state.counter++;
+    increment: (state, payload) => {
+      state.counter += payload;
     },
-    decrement: state => {
-      state.counter--;
+    decrement: (state, payload) => {
+      state.counter -= payload;
     }
   },
   actions: {
-    increment: context => {
-      context.commit("increment");
+    increment: (context, payload) => {
+      context.commit("increment", payload);
     },
-    decrement: ({ commit }) => {
-      commit("decrement");
+    decrement: ({ commit }, payload) => {
+      commit("decrement", payload);
     },
-    asyncIncrement: ({ commit }) => {
+    asyncIncrement: ({ commit }, payload) => {
       setTimeout(() => {
-        commit("increment");
-      }, 1000);
+        commit("increment", payload.by);
+      }, payload.duration);
     },
-    asyncDecrement: ({ commit }) => {
+    asyncDecrement: ({ commit }, payload) => {
       setTimeout(() => {
-        commit("decrement");
-      }, 1000);
+        commit("decrement", payload.by);
+      }, payload.duration);
     }
   }
 });
